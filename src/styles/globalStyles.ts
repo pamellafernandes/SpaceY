442404cc185111ee48eb0ca350aa0b15743576a2
pdlima <pdlima@CPQI-NBK-92>
2022-04-02 02:16:46 -0300

    Space Y

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+:root {
+    --space-light: #3D68B2;
+    --space-dark: #040327;
+    --space: #0B1E8A;
+    --gray-01: #0D0E13;
+    --gray02: #2C2D3A;
+    --gray-03: #60616F; 
+    --gray-04: #898A93;
+    --gray-05: #D5D5DB;
+    --mars-light: #FF8C70;
+}
+`;
