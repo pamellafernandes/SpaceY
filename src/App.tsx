fc2e5fcd03336cd9be34f2560bec921e8af6e459
pdlima <pdlima@CPQI-NBK-92>
2022-04-05 00:53:50 -0300

    Space Y

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import { GlobalStyles } from "./styles/globalstyles";
-import { Header } from "./styles/styles";
+import { Header, Logo, Main, Title, Subtitle } from "./styles/styles";
 import Button from "./components/Button";
 
 function App() {
     return (
         <>
             <GlobalStyles />
+
             <Header>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
-                nostrum molestiae asperiores nam eum, qui tempora quis in fuga
-                vel sint nemo neque reiciendis explicabo architecto ad adipisci,
-                blanditiis sequi.
+                <Logo>
+                    <img src="/images/logo-space-y.svg" alt="" />
+                </Logo>
             </Header>
 
+            <Main>
+                <p>Finalmente é possível</p>
+
+                <Title>
+                    Sua jornada para Marte começa aqui <span>.</span>
+                </Title>
+
+                <Subtitle>
+                    A primeira viagem para Marte estará disponivel apartir do dia 12/03/2028.
+                    Inscreva-se em nossa lista de espera.
+                </Subtitle>
+            </Main>
             <Button text="Inscreva-se agora" />
         </>
     );
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
+function pixelToRem(...values: number[]) {
+    return values.reduce((acc, current) => (acc += current / 16 + `rem `), "").trim();
+}
+
 export const Header = styled.div`
     display: flex;
-    /* background: url("/images/logo.svg"); */
     width: 100%;
-    font: var(--font-display);
+    background-color: var(--space);
 `;
+export const Logo = styled.image`
+    width: pixelToRem(201);
+    height: pixelToRem(41);
+`;
+
+export const Main = styled.div``;
+
+export const Title = styled.div``;
+
+export const Subtitle = styled.div``;
