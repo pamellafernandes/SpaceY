df30884cb53de39edf042ae67081769e8b699fa8
pdlima <pdlima@CPQI-NBK-92>
2022-04-06 22:04:21 -0300

    Space Y

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -14,10 +14,10 @@ export const ButtonComponent = styled.button<ButtonProps>`
     color: var(--text);
     font: var(--text-3);
     font-weight: 700;
-    transition: all 0.5s ease-in-out;
+    transition: all ease-in-out 0.5s;
 
     &:hover {
-        background: var(--gray-01);
+        filter: brightness(0.8);
         cursor: pointer;
     }
 `;
